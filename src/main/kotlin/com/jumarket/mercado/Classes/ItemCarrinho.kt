646b44427120ea60a0d9ba45bdcfa6b5dc6a5993
matmagc <thiago.h.ceolin@gmail.com>
2023-07-13 10:51:01 -0300

    first commit

---
+++ src/main/kotlin/com/jumarket/mercado/Classes/ItemCarrinho.kt
@@ -0,0 +1,5 @@
+package com.jumarket.mercado.Classes
+
+// Classe para representar o carrinho de compras
+
+data class ItemCarrinho(val produto: Produto, val quantidade: Int)
---
+++ src/main/kotlin/com/jumarket/mercado/Classes/Produto.kt
@@ -0,0 +1,5 @@
+package com.jumarket.mercado.Classes
+
+// Classe para representar um produto
+
+data class Produto(val nome: String, val unidade: String, val preco: Double)
